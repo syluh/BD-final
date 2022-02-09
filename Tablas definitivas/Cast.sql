4357626e9973c794e8ca8745d12cd9d945568a82
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:36:08 -0500

    Update Cast.sql

--- Tablas definitivas/Cast.sql
+++ Tablas definitivas/Cast.sql
@@ -1,11 +1,12 @@
 -- Cast DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_Cast;
 CREATE TABLE movie_Cast
 SELECT id_movie, md5(`name`) AS id_cast 
 FROM tmp_cast
 GROUP BY md5(`name`);
 
-
+-- Primera forma normal
 DROP TABLE IF EXISTS Cast;
 CREATE TABLE Cast
 SELECT DISTINCT md5(`name`) AS id_cast , `name` 
