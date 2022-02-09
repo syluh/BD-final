e98a9a82c4702bf67c2b0ad3e4504071635cd8ab
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:38:49 -0500

    Update Genres.sql

--- Tablas definitivas/Genres.sql
+++ Tablas definitivas/Genres.sql
@@ -1,10 +1,12 @@
 -- Genres DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_Genres;
 CREATE TABLE movie_Genres
 SELECT id_movie, md5(genres) AS genreID 
 FROM tmp_genres
 WHERE genres != '""';
 
+-- Segunda forma normal
 DROP TABLE IF EXISTS genres;
 CREATE TABLE genres
 SELECT DISTINCT  md5(genres) AS genreID, genres
