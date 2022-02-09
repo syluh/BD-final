c48451846d0d175c331ef6a36717192a3901cb05
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:40:24 -0500

    Update keywords.sql

--- Tablas definitivas/keywords.sql
+++ Tablas definitivas/keywords.sql
@@ -1,4 +1,5 @@
 -- key words DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_keywords;
 CREATE TABLE movie_keywords
 SELECT id_movie, md5(keyWords) AS id_keywords
@@ -6,7 +7,7 @@ FROM tmp_key_words
 WHERE keyWords != '""'
 GROUP BY md5(keyWords);
 
-
+-- Segunda forma Normal
 DROP TABLE IF EXISTS keywords;
 CREATE TABLE keywords
 SELECT DISTINCT md5(keyWords)AS id_keywords,keyWords
