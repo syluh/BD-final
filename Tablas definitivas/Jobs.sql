c5b03efccad6df3bb8d888ec368d1763e2f5a5cb
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:39:09 -0500

    Update Jobs.sql

--- Tablas definitivas/Jobs.sql
+++ Tablas definitivas/Jobs.sql
@@ -1,5 +1,5 @@
 -- Jobs DEFINITVO
-
+-- Tercera forma normal a la entidad crew
 DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs
 SELECT DISTINCT md5(job) AS idJob, job, md5(department) AS id_department 
