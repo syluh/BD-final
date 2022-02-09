7ecad8cba8269c3fdb695d8288c0a0a78844c683
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:40:55 -0500

    Update Pcountries.sql

--- Tablas definitivas/Pcountries.sql
+++ Tablas definitivas/Pcountries.sql
@@ -1,10 +1,11 @@
 -- Production Countries DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_Pcountries;
 CREATE TABLE movie_Pcountries
 SELECT id_movie, iso_3166_1 
 FROM tmp_production_countries;
 
-
+-- Segunda forma normal
 DROP TABLE IF EXISTS production_countries;
 CREATE TABLE production_countries
 SELECT DISTINCT `name`, iso_3166_1
