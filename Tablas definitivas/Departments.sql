970cf317dd56b6fc6b34d0beef6a0e3a8c5039bd
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:36:52 -0500

    Update Departments.sql

--- Tablas definitivas/Departments.sql
+++ Tablas definitivas/Departments.sql
@@ -1,5 +1,5 @@
 -- Departments DEFINITVO
-
+-- Tercera forma normal a la entidad Crew
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments
 SELECT DISTINCT md5(department) AS id_department, department
