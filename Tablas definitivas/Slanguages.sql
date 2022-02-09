7eb973f11747977262d16fa67a8b45acb91a4294
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:41:10 -0500

    Update Slanguages.sql

--- Tablas definitivas/Slanguages.sql
+++ Tablas definitivas/Slanguages.sql
@@ -1,11 +1,12 @@
 -- Spoken Languages DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_Slanguages;
 CREATE TABLE movie_Slanguages
 SELECT id_movie, iso_639_1 
 FROM tmp_spoken_languages
 WHERE `name` != '""';
 
-
+-- Segunda forma Normal
 DROP TABLE IF EXISTS spoken_Langugages;
 CREATE TABLE spoken_Langugages
 SELECT DISTINCT `name`,iso_639_1
