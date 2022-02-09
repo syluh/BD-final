a70c9a0a533d6cab16954ef919ec785f99248ebb
vecarrion <73541103+syluh@users.noreply.github.com>
2022-02-09 08:40:38 -0500

    Update Pcompanies.sql

--- Tablas definitivas/Pcompanies.sql
+++ Tablas definitivas/Pcompanies.sql
@@ -1,10 +1,11 @@
 -- Production Companies DEFINITIVO
+-- Solución de la cardinalidad n:n
 DROP TABLE IF EXISTS movie_pCompanies;
 CREATE TABLE movie_pCompanies
 SELECT id_movie, id_production_companies 
 FROM tmp_production_companies;
 
-
+-- Segunda forma normal
 DROP TABLE IF EXISTS production_companies;
 CREATE TABLE production_companies
 SELECT DISTINCT id_production_companies, `name`
